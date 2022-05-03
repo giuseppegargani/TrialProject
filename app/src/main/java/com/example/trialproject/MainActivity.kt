a9d80e4b00031a5aeaff4a9dc2c16cd56867e122
giuseppegargani <sentierodisole1@yahoo.it>
2022-05-03 17:53:37 +0200

    Initial commit

---
+++ app/src/main/java/com/example/trialproject/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.trialproject
+
+/* REPOSITORY TRIAL FINALE DOVE PROVARE LE LIBRERIE CHE VENGONO CREATE
+    Quindi devono avere solo test strumentali e non
+    Proviamo anche a mettere un semplice caricamento di libreria da progetto in un file yaml!!!
+    direttamente da IDE
+ */
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
